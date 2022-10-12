4445aa3c4bcca3fedea7bf160148961dc4cce916
liangxu <whitebrow_dev@163.com>
2022-10-12 15:17:21 +0800

    fix: mac端m1编译x64

--- all/sharedlib/template/generate_qt_osx_project.sh
+++ all/sharedlib/template/generate_qt_osx_project.sh
@@ -6,9 +6,9 @@ bash_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "generate qt macos project ..."
 
-qt_osx_path="/Users/liuxiufeng/Qt5.12.12/5.12.12/clang_64"
+qt_osx_path="/Users/xiufeng.liu/Qt/5.15.2/clang_64"
 qt_osx_dir="$bash_dir/build/macos_qt/apps/sharedlib_name_app"
-cmake -S "$bash_dir/apps/sharedlib_name_app/qt/SharedlibNameApp" -B "$qt_osx_dir" -G "Xcode" -DMACOS:BOOL=ON -DCMAKE_PREFIX_PATH=${qt_osx_path}
+cmake -S "$bash_dir/apps/sharedlib_name_app/qt/SharedlibNameApp" -B "$qt_osx_dir" -G "Xcode" -DCMAKE_OSX_ARCHITECTURES=x86_64 -DMACOS:BOOL=ON -DCMAKE_PREFIX_PATH=${qt_osx_path}
 
 app_xcodeproj="$bash_dir/QtApp.xcodeproj"
 if ! [ -h "$app_xcodeproj" ]; then
