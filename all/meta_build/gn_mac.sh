1484326ab87a64c98bcd7b93452709b410d726ec
liangxu <whitebrow_dev@163.com>
2022-10-12 18:56:23 +0800

    feat: meta_build使用gn

---
+++ all/meta_build/gn_mac.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#
+
+bash_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+repo_dir="${bash_dir}/../.."
+
+gn="${repo_dir}/tools/gn/mac/gn"
+out_dir="${bash_dir}/out"
+gn_args="is_debug=true is_official_build=false"
+
+${gn} gen "${out_dir}" --ide=xcode --xcode-build-system=new --xcode-project=meta_build --args="${gn_args}" 
---
+++ all/meta_build/tutorial/tutorial.cc
@@ -0,0 +1,9 @@
+// Copyright 2022 The meta_build Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+  printf("Hello from the tutorial.\n");
+  return 0;
+}
