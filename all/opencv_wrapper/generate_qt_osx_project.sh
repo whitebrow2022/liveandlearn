f32063aeeb36e4ccb0de5106c2a3a4c2dafee260
liangxu <whitebrow_dev@163.com>
2022-10-24 09:55:57 +0800

    feat: opencv mac端使用静态库

--- all/opencv_wrapper/generate_qt_osx_project.sh
+++ all/opencv_wrapper/generate_qt_osx_project.sh
@@ -8,7 +8,7 @@ echo "generate qt macos project ..."
 
 qt_osx_path="/Users/xiufeng.liu/Qt/5.15.2/clang_64"
 qt_osx_dir="$bash_dir/build/macos_qt/apps/opencv_wrapper_app"
-cmake -S "$bash_dir/apps/opencv_wrapper_app/qt/OpencvWrapperApp" -B "$qt_osx_dir" -G "Xcode" -DCMAKE_OSX_ARCHITECTURES=x86_64 -DMACOS:BOOL=ON -DCMAKE_PREFIX_PATH=${qt_osx_path} -DBUILD_opencv_dnn=OFF -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_opencv_apps=OFF -DBUILD_PERF_TESTS=OFF
+cmake -S "$bash_dir/apps/opencv_wrapper_app/qt/OpencvWrapperApp" -B "$qt_osx_dir" -G "Xcode" -DCMAKE_OSX_ARCHITECTURES=x86_64 -DMACOS:BOOL=ON -DCMAKE_PREFIX_PATH=${qt_osx_path} -DBUILD_opencv_dnn=OFF -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_opencv_apps=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_SHARED_LIBS=OFF
 
 app_xcodeproj="$bash_dir/QtApp.xcodeproj"
 if ! [ -h "$app_xcodeproj" ]; then
